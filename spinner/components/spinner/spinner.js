06024609cecdaa6c01eab2b8da81052aee204c2c
Håkon Mosvoll <hakon.mosvoll@payex.com>
2021-11-26 13:38:22 +0100

    Spinner circles based on attributes.

--- spinner/components/spinner/spinner.js
+++ spinner/components/spinner/spinner.js
@@ -4,24 +4,28 @@ class Spinner extends HTMLElement {
 
         const $shadow = this.attachShadow({mode: "open"});
 
-        const $ul = document.createElement("ul");
-        $ul.classList.add("spinner")
-
-        const $li = document.createElement("li");
-        $ul.appendChild($li);
-
-        const $liTwo = document.createElement("li");
-        $ul.appendChild($liTwo);
-
-        $shadow.append($ul)
-
         const $link = document.createElement("link");
         $link.setAttribute("rel", "stylesheet");
         $link.setAttribute("href", "components/spinner/spinner.css");
 
         $shadow.append($link);
-    }
 
+        const $ul = document.createElement("ul");
+        $ul.classList.add("spinner")
+
+        const numberOfCircles = this.getAttribute("circles");
+
+        let i = 0;
+        for(i; i < numberOfCircles; i++){
+            const $li = document.createElement("li");
+            $ul.appendChild($li);
+        }
+
+        $shadow.append($ul)
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log("attributeChangedCallback: ", name, oldValue, newValue);
+    }
 }
 
 customElements.define("spinner-deluxe", Spinner);
